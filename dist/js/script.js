158a0e39118a5c8c841dafc147a41fd9f63cc41c
Bassam AL SAEDy <basaidi@gmail.com>
2020-12-18 19:17:50 +0100

    New project adde!

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,10 +2,19 @@
 const header = document.querySelector(".main-header");
 
 window.addEventListener("scroll", () => {
-  const scrollPos = window.scrollY;
-  if (scrollPos > 2) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
+    const scrollPos = window.scrollY;
+    if (scrollPos > 2) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
 });
+
+function hideMenu() {
+    let menuOpen = document.querySelector(".menu-btn").checked;
+
+    if ((menuOpen = true)) {
+        document.querySelector(".menu-btn").checked = false;
+    }
+}
+window.addEventListener("scroll", hideMenu);
